8f31105e5643a1ffbbe14ff075b705fbb84513af
Stanley Huang <huang.stanley050@gmail.com>
2020-03-30 16:29:48 +1100

    refactored create.js

--- services/notes/create.js
+++ services/notes/create.js
@@ -1,7 +1,6 @@
 import uuid from "uuid";
-import AWS from "aws-sdk";
-
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
 
 export const main = async (event, context) => {
   // Request body is passed in as a JSON encoded string in 'event.body'
@@ -19,30 +18,10 @@ export const main = async (event, context) => {
     }
   };
 
-  dynamoDb.put(params, (error, data) => {
-    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    };
-
-    // Return status code 500 on error
-    if (error) {
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({ status: false })
-      };
-
-      return response;
-    }
-
-    // Return status code 200 and the newly created item
-    const response = {
-      statusCode: 200,
-      headers: headers,
-      body: JSON.stringify(params.Item)
-    };
-    return response;
-  });
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
 };
