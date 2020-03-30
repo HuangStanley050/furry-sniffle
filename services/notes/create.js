4619d6e982cbd0a59e48f293dabca002cf1d5957
Stanley Huang <huang.stanley050@gmail.com>
2020-03-30 16:32:34 +1100

    fixed path issue in create.js

--- services/notes/create.js
+++ services/notes/create.js
@@ -1,6 +1,6 @@
 import uuid from "uuid";
-import * as dynamoDbLib from "./libs/dynamodb-lib";
-import { success, failure } from "./libs/response-lib";
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
 
 export const main = async (event, context) => {
   // Request body is passed in as a JSON encoded string in 'event.body'
